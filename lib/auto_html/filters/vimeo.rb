d96b6d8afb061cb75d822e8e19528d97b36be729
Michal Szyndel <msz@sent.com>
2014-09-22 11:28:04 +0200

    vimeo extra options

--- lib/auto_html/filters/vimeo.rb
+++ lib/auto_html/filters/vimeo.rb
@@ -6,10 +6,12 @@ AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => f
     show_title      = "title=0"    unless options[:show_title]
     show_byline     = "byline=0"   unless options[:show_byline]  
     show_portrait   = "portrait=0" unless options[:show_portrait]
+    api             = "api=1"      if options[:api]
+    id = "player_id=#{options[:id]}" if options[:id]
     frameborder     = options[:frameborder] || 0
-    query_string_variables = [show_title, show_byline, show_portrait].compact.join("&")
+    query_string_variables = [show_title, show_byline, show_portrait, api, id].compact.join("&")
     query_string    = "?" + query_string_variables unless query_string_variables.empty?
 
-    %{<iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"></iframe>}
+    %{<div class="video vimeo"><iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" id="#{options[:id]}" frameborder="#{frameborder}"></iframe></div>}
   end
 end
